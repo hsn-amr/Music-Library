0f9976ac5ee26a33b6e92767d9b34461139a3b67
Hassan Al-Shammari <hsn999hsn@gmail.com>
2022-01-17 02:13:40 -0800

    Initial Commit

---
+++ Music Library/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Music Library
+//
+//  Created by administrator on 17/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Music Library/Music_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_LibraryApp.swift
+//  Music Library
+//
+//  Created by administrator on 17/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Music_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
